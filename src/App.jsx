40ddda147c12dee4442471d0c3ff38e3fb653d82
Melqui42 <melquisedeque.martins42@gmail.com>
2023-01-26 00:13:51 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,270 @@
+import React from "react";
+
+import { BiUser, BiMailSend } from "react-icons/bi";
+import {
+  IoMdClose,
+  IoIosArrowForward,
+  IoIosArrowBack,
+  IoIosMenu,
+} from "react-icons/io";
+import { BsBriefcase, BsFolderCheck } from "react-icons/bs";
+import { AiOutlineDownload } from "react-icons/ai";
+
+import foguete from "./Assets/foguete.png";
+import minhaFoto from "./Assets/Screenshot_3.png";
+
+import { habilidades, projetos, redesSociais, itensDeNavegacao } from "./Data";
+
+import "./Style.scss";
+
+export default function App() {
+  const [dropDown, setDropDown] = React.useState(false);
+  const [menuMobile, setMenuMobile] = React.useState(false);
+  const [windowWelcome, setWindowWelcome] = React.useState(false);
+
+  const handleClick = (e) => {
+    e.preventDefault();
+
+    const target = e.target.getAttribute("href");
+    const location = document.querySelector(target).offsetTop;
+
+    window.scrollTo({
+      left: 0,
+      top: location - 120,
+    });
+  };
+
+  return (
+    <div className="App">
+      <header>
+        <nav className="NavContainer">
+          <menu className="Content">
+            <a href="#Home" className="Logo">
+              <img src={foguete} alt="" />
+              <div>
+                <h1>Melqui Martins</h1>
+                <p>Rumo ao sucesso🔰</p>
+              </div>
+            </a>
+            <menu>
+              <ul className={!menuMobile ? "HideList" : "ShowList"}>
+                {itensDeNavegacao.map((Item) => {
+                  return (
+                    <li className="Item" key={Item.id}>
+                      <a href={Item.link} onClick={handleClick}>
+                        {Item.title}
+                      </a>
+                    </li>
+                  );
+                })}
+              </ul>
+              <button onClick={() => setMenuMobile(!menuMobile)}>
+                <IoIosMenu />
+              </button>
+            </menu>
+          </menu>
+          <menu className={!windowWelcome ? "ShowOthers" : "HideOthers"}>
+            <div className="Center">
+              <p>Seja bem-vindo(a) ao meu Portfolio!!! 👋</p>
+              <button onClick={() => setWindowWelcome(!windowWelcome)}>
+                <IoMdClose />
+              </button>
+            </div>
+          </menu>
+        </nav>
+      </header>
+      <section className="SectionHome" id="Home">
+        <div className="Center">
+          <article>
+            <span>Desenvolvedor Front-End</span>
+            <h1>Olá, eu sou Melqui Martins!</h1>
+            <p>
+              Olá, me chamo Melquisedeque mas podem me chamar de Melqui, tenho
+              17 anos e iniciei meus estudos com programação em 2020 e ao longo
+              desse tempo eu tenho estudado tecnologias voltadas para o Front
+              End.
+            </p>
+            <ul className="List">
+              <li className="Item">
+                <button className="DownloadCV">
+                  Download CV <AiOutlineDownload className="Icon" />
+                </button>
+              </li>
+              <li className="Item">
+                <button
+                  className="ButtonDropDown"
+                  onClick={() => setDropDown(!dropDown)}
+                >
+                  {!dropDown ? (
+                    <IoIosArrowForward className="Icon" />
+                  ) : (
+                    <IoIosArrowBack className="Icon" />
+                  )}
+                </button>
+
+                <ul
+                  className={
+                    !dropDown ? "HideDropDownList" : "ShowDropDownList"
+                  }
+                >
+                  {redesSociais.map((Item) => {
+                    return (
+                      <li className="Item" key={Item.id} onClick={Item.metodo}>
+                        <a href={Item.link} target="_blank" rel="noreferrer">
+                          {Item.img}
+                        </a>
+                      </li>
+                    );
+                  })}
+                </ul>
+              </li>
+            </ul>
+          </article>
+          <img src={minhaFoto} alt="" />
+        </div>
+      </section>
+      <section className="SectionAboutMe" id="AboutMe">
+        <div className="Center">
+          <div className="Title">
+            <h1>Sobre Mim</h1>
+            <p>~ Introdução ~</p>
+          </div>
+          <article>
+            <div className="Others">
+              <div className="FirstContent">
+                <h1>Quer falar comigo?</h1>
+                <p>
+                  Eu tenho algumas redes sociais como: Instagram, Linkedin e
+                  WhatsApp
+                </p>
+
+                <ul className="List">
+                  {redesSociais.map((Item) => {
+                    return (
+                      <li className="Item" key={Item.id}>
+                        <a
+                          href={Item.link}
+                          target="_blank"
+                          rel="noreferrer"
+                          onClick={Item.metodo}
+                        >
+                          {Item.img}
+                        </a>
+                      </li>
+                    );
+                  })}
+                </ul>
+
+                <p>
+                  Você tambem pode se comunicar comigo através do meu email.
+                </p>
+                <button
+                  className="DownloadCV"
+                  onClick={() =>
+                    window.alert("melquisedeque.martins42@gmail.com")
+                  }
+                >
+                  Entre em contato comigo!!! <BiMailSend className="Icon" />
+                </button>
+              </div>
+
+              <div className="SecondContent">
+                <h1>A frase que me motiva:</h1>
+                <p>“É em meio a dificuldade que se encontra a oportunidade.”</p>
+              </div>
+            </div>
+            <div className="Content">
+              <ul className="List">
+                <li className="Item">
+                  <div>
+                    <BsBriefcase className="Icon" />
+                    <h1>Experiência</h1>
+                    <p>2 Anos de estudos</p>
+                  </div>
+                </li>
+                <li className="Item">
+                  <div>
+                    <BsFolderCheck className="Icon" />
+                    <h1>Projetos</h1>
+                    <p>3 + Completados</p>
+                  </div>
+                </li>
+                <li className="Item">
+                  <div>
+                    <BiUser className="Icon" />
+                    <h1>Suporte</h1>
+                    <p>8/5</p>
+                  </div>
+                </li>
+              </ul>
+              <p className="Text">
+                Tenho foco em desenvolvimento com JavaScript, ReactJS e Saas,
+                também tenho estudado tecnologias voltadas para o Back End como
+                o Node JS.
+              </p>
+              <p className="Text">
+                Me considero ágil para aprender uma nova Linguagem de
+                Programação, estou sempre me preparando para superar novos
+                desafios e concluir meus objetivos e estou ansioso para integrar
+                na minha primeira oportunidade de trabalho, onde eu posso colher
+                conhecimentos e possa aplicar minhas habilidades, se eu pudesse
+                me definir em 2 palavras, certamente seria: esforçado e
+                otimista.
+              </p>
+            </div>
+          </article>
+        </div>
+      </section>
+      <section className="SectionSkills" id="Skills">
+        <div className="Center">
+          <div className="Title">
+            <h1>Habilidades</h1>
+            <p>~ Tecnologias que estudo ~</p>
+          </div>
+          <article>
+            <ul className="List">
+              {habilidades.map((Item) => {
+                return (
+                  <li className="Item" key={Item.id}>
+                    <div>
+                      <img src={Item.img} alt="" />
+                      <h1>{Item.title}</h1>
+                      <p>{Item.text}</p>
+                    </div>
+                  </li>
+                );
+              })}
+            </ul>
+          </article>
+        </div>
+      </section>
+      <section className="SectionProjects" id="Projects">
+        <div className="Center">
+          <div className="Title">
+            <h1>Meus Projetos</h1>
+            <p>~ Principais projetos ~</p>
+          </div>
+          <article>
+            <ul className="List">
+              {projetos.map((Item) => {
+                return (
+                  <li className="Item" key={Item.id}>
+                    <a href={Item.link} target="_blank" rel="noreferrer">
+                      <div className="Card">
+                        <img src={Item.img} alt="" />
+                        <div>
+                          <h1>{Item.title}</h1>
+                          <p>{Item.text}</p>
+                        </div>
+                      </div>
+                    </a>
+                  </li>
+                );
+              })}
+            </ul>
+          </article>
+        </div>
+      </section>
+    </div>
+  );
+}
---
+++ src/Data.js
@@ -0,0 +1,115 @@
+import JavaScript_Icone from "./Assets/JavaScript_Icone.png";
+import ReactJS_Icone from "./Assets/ReactJS_Icone.png";
+import NodeJS_Icone from "./Assets/NodeJS_Icone.png";
+import TypeScript_Icone from "./Assets/TypeScript_Icone.png";
+import Sass_Icone from "./Assets/Sass_Icone.png";
+import MySQL_Icone from "./Assets/MySQL.png";
+
+import ToDoList from "./Assets/ToDoList.png";
+import BuscadorDeCep from "./Assets/BuscadorDeCep.png";
+import ControleDeFinancas from "./Assets/ControleDeFinanças.png";
+
+import {
+  AiFillLinkedin,
+  AiFillGithub,
+  AiOutlineWhatsApp,
+} from "react-icons/ai";
+
+export const habilidades = [
+  {
+    id: 1,
+    img: ReactJS_Icone,
+    title: "React JS",
+    text: "React JS é tecnologia que eu tenho focado ultimamente, estou estudando sobre renderização e também estou criando pequenos projetos para colocar no meu github e linkedin.",
+  },
+  { id: 2, img: Sass_Icone, title: "Sass", text: "sem descrição." },
+  {
+    id: 3,
+    img: JavaScript_Icone,
+    title: "JavaScript",
+    text: "Bom JavaScript é a tecnologia eu tenho mais conhecimento, isso é fundamental já que as outras tecnologias que eu uso partem dela, eu poderia dizer que eu estou em um nivel intermediario.",
+  },
+  {
+    id: 4,
+    img: TypeScript_Icone,
+    title: "TypeScript",
+    text: "Eu ainda estou estudando aos poucos o TypeScript, ainda preciso construir uma base solida no JavaScript para não ter muitas dificuldades, eu quero usar o TypeScript junto com o React JS.",
+  },
+  {
+    id: 5,
+    img: NodeJS_Icone,
+    title: "Node JS",
+    text: "Node JS não é meu foco atualmente, mas eu quero estudar para conseguir entender como funciona uma API e para melhorar o meu consumo de APIs.",
+  },
+  { id: 6, img: MySQL_Icone, title: "MySQL", text: "sem descrição." },
+];
+
+export const projetos = [
+  {
+    id: 1,
+    img: ControleDeFinancas,
+    title: "Controle de finanças",
+    text: "Projeto desenvolvido em ReactJS, Sass e LocalStorage. (sem uso de frameworks)",
+    link: "https://github.com/Melqui42/Controle-Financeiro",
+  },
+  {
+    id: 2,
+    img: BuscadorDeCep,
+    title: "Buscador de CEP",
+    text: "Projeto desenvolvido em ReactJS, Sass e Axios",
+    link: "https://github.com/Melqui42/Buscador-de-CEP",
+  },
+  {
+    id: 3,
+    img: ToDoList,
+    title: "Lista de Tarefas",
+    text: "Projeto desenvolvido em ReactJS, Sass (sem uso de frameworks)",
+    link: "https://github.com/Melqui42/To-do-List",
+  },
+];
+
+export const redesSociais = [
+  {
+    id: 1,
+    img: <AiFillLinkedin />,
+    title: "Linkedin",
+    link: "https://www.linkedin.com/in/melquisedeque-martins-54108a25a/",
+  },
+  {
+    id: 2,
+    img: <AiFillGithub />,
+    title: "Github",
+    link: "https://github.com/Melqui42",
+  },
+  {
+    id: 3,
+    img: <AiOutlineWhatsApp />,
+    title: "WhatsApp",
+    metodo: function () {
+      window.alert("+55 81 98273-0592");
+    },
+  },
+];
+
+export const itensDeNavegacao = [
+  {
+    id: 1,
+    title: "Início",
+    link: "#Home",
+  },
+  {
+    id: 2,
+    title: "Sobre Mim",
+    link: "#AboutMe",
+  },
+  {
+    id: 3,
+    title: "Habilidades",
+    link: "#Skills",
+  },
+  {
+    id: 4,
+    title: "Projetos",
+    link: "#Projects",
+  },
+];
